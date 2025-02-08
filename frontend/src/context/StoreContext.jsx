928366140738b01e833d75519b6154a36aa6284a
Utkarshnirmal11 <nirmalsushant004@gmail.com>
2025-02-08 00:05:38 -0800

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems] = useState({});
-    const url="https://khushi-s-dine-1.onrender.com";
+    const url="https://food-del-backend-5knm.onrender.com";
     const [token,setToken]=useState("")
 
     const [food_list,setFoodList] = useState([]);
